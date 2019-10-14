11a88edff60be47bfab2727098cd9190b5c53d55
Daniel Mahal <danielmahal@gmail.com>
2019-10-14 15:35:13 +0200

    Scroll hack

--- assets/focus.js
+++ assets/focus.js
@@ -1,23 +1,25 @@
+const scroller = document.querySelector('.scroller')
+const spacer = document.querySelector('.spacer')
 const elements = Array.from(document.querySelectorAll('[data-depth]'))
 const depths = elements.map(element => parseFloat(element.getAttribute('data-depth'), 10))
 
 let request
 
-window.addEventListener('scroll', event => {
+scroller.addEventListener('scroll', function () {
   if (!request) {
     request = requestAnimationFrame(update)
   }
 })
 
 function update () {
-  const scroll = document.documentElement.scrollTop
-  const contentHeight = document.body.offsetHeight
+  const scroll = scroller.scrollTop
+  const contentHeight = spacer.offsetHeight
   const windowHeight = window.innerHeight
   const position = scroll / (contentHeight - windowHeight)
 
   elements.forEach((element, index) => {
     const depth = depths[index]
-    const focus = (Math.pow(Math.abs(position - depth) * 15, 2) / 10).toFixed(1)
+    const focus = Math.min((Math.pow(Math.abs(position - depth) * 15, 2) / 10), 10).toFixed(1)
     element.style.filter = `blur(${focus}px)`
   })
 
@@ -25,3 +27,11 @@ function update () {
 }
 
 update()
+
+window.addEventListener('resize', updateHeight)
+
+function updateHeight () {
+  scroller.style.height = `${window.innerHeight - 1}px`
+}
+
+updateHeight()
